8034d4b19f53823de53e7b35ca8dfdc431a58d7a
Klezovich <olegolegolegoleg88@gmail.com>
2024-01-21 20:42:38 +0300

    lesson51

---
+++ lesson51/src/test/java/org/tms/Lesson51ApplicationTest.java
@@ -0,0 +1,10 @@
+package org.tms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson51ApplicationTest {
+    @Test
+    void contextLoads() {}
+}
